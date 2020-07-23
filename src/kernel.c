c386278b95e66213b96306d718084e71067e161e
frozennothing <ndieckhaus@corecreationstudios.net>
2020-07-23 16:26:00 +0800

    Cleaned up and commented some code

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,7 @@
 //These are the CONSTS for the memory management
+//There is currently no memory management in this code, but if there SHOULD be memory
+//these consts should point to where various memory is
+//For example, the start and end of the heap
 #define _P_BASE_H
 
 #define PBASE 0x3F000000
@@ -15,19 +18,13 @@
 #define PAGING_MEMORY (HIGH_MEMORY - LOW_MEMORY)
 #define PAGING_PAGES (PAGING_MEMORY / PAGE_SIZE)
 
+//The sorts
 #include "mergeSort.h"
 #include "quickSort.h"
 #include "heapSort.h"
 #include "bubbleSort.h"
 #include <stddef.h>
-//#include "tinyalloc.h"
 
-/*
-//These are the options for the sorting algos
-#define SORT_NAME int64
-#define SORT_TYPE int64_t
-#define SORT_CMP(x, y) ((x) - (y))
-#include "sort.h" */
 //This is for the connection to the uart
 #include "mini_uart.h"
 #include "printf.h"
@@ -37,15 +34,15 @@
 #define NUMLENGTH 100000
 #define RUNS 20
 
-//These are the helper functions that the sorting algos need to run!
+
 // This function is required by printf function
+//It put prints characters to the console
 void putc(void *p, char c)
 {
   uart_send(c);
 }
 
-void *
-memcpy(void *dest, const void *src, size_t len)
+void * memcpy(void *dest, const void *src, size_t len)
 {
   char *d = dest;
   const char *s = src;
@@ -53,58 +50,7 @@ memcpy(void *dest, const void *src, size_t len)
     *d++ = *s++;
   return dest;
 }
-/*void *realloc(void *ptr, size_t size)
-{
-  void *new_data = NULL;
 
-  if (size)
-  {
-    if (!ptr)
-    {
-      return ta_alloc(size);
-    }
-
-    new_data = ta_alloc(size);
-    if (new_data)
-    {
-      memcpy(new_data, ptr, size); // TODO: unsafe copy...
-      ta_free(ptr);                // we always move the data. free.
-    }
-  }
-
-  return new_data;
-}
-
-void *reallocf(void *ptr, size_t size)
-{
-  void *p = realloc(ptr, size);
-
-  if ((p == NULL) && (ptr != NULL))
-  {
-    ta_free(ptr);
-  }
-
-  return p;
-}
-
-void *
-memmove(void *dest, const void *src, size_t len)
-{
-  char *d = dest;
-  const char *s = src;
-  if (d < s)
-    while (len--)
-      *d++ = *s++;
-  else
-  {
-    char *lasts = s + (len - 1);
-    char *lastd = d + (len - 1);
-    while (len--)
-      *lastd-- = *lasts--;
-  }
-  return dest;
-}
-*/
 void swap(int* a, int* b)  
 {  
     int t = *a;  
@@ -112,25 +58,14 @@ void swap(int* a, int* b)
     *b = t;  
 }  
   
-/*
-size_t int2size_t(int val) {
-    return (val < 0) ? __SIZE_MAX__ : (size_t)((unsigned)val);
-}
-*/
-
+//This is the main program, it runs this
 void kernel_main(void)
 {
 
-  uart_init();
-  init_printf(0, putc);
+  uart_init(); //Starts the connection to UART
+  init_printf(0, putc); //Inits the printf, must be run before using printf
   printf("Starting\n");
   int dst[NUMLENGTH];
-  //size_t split_thresh = 16;
-  //size_t heap_blocks = 256;
-  //size_t alignment = 64;
-  //printf("initing heap\n");
-  //ta_init(LOW_MEMORY, HIGH_MEMORY, heap_blocks, split_thresh, alignment);
-
   //printf("entering for loop\n");
   for (int i = 0; i < RUNS; i++)
   {
